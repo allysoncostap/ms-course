a97a145a8b4fe101a8a8362ee0753d22404064d3
Allyson Costa <costa.allyson@gmail.com>
2022-11-09 18:25:58 -0300

    hr-workers implementation

---
+++ hr-worker/hr-worker/src/main/java/com/allyson/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,7 @@
+package com.allyson.hrworker.repositories;
+
+import com.allyson.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker,Long> {
+}
