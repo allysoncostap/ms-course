010bb20ea7db6cd9bfd1c6d5075936fed3869869
Allyson Costa <costa.allyson@gmail.com>
2022-11-10 21:28:25 -0300

    Feing client

--- hr-payroll/src/main/java/com/allyson/hrpayroll/services/PaymentServices.java
+++ hr-payroll/src/main/java/com/allyson/hrpayroll/services/PaymentServices.java
@@ -3,24 +3,25 @@ package com.allyson.hrpayroll.services;
 
 import com.allyson.hrpayroll.entities.Payment;
 import com.allyson.hrpayroll.entities.Worker;
+import com.allyson.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.stereotype.Service;
+
+
+
+
 
 @Service
 public class PaymentServices {
-    @Value("${hr-worker.host}")
-private String workerHost;
-    @Autowired
-    private RestTemplate restTemplate;
+
+
+@Autowired
+private WorkerFeignClient workerFeignClient;
+
     public Payment getPayment(long workerId, int days){
-Map<String,String> uriVariables = new HashMap<>();
-uriVariables.put("id",""+workerId);
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class,uriVariables);
+    Worker worker = workerFeignClient.findById(workerId).getBody();
+
         return new Payment(worker.getName(),worker.getDailyIncome(),days);
     }
 }
