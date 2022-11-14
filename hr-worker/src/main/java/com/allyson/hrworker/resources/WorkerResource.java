d279b115d64ecd35207385d3f847c6e8046bed0e
Allyson Costa <costa.allyson@gmail.com>
2022-11-14 20:32:18 -0300

    Actuator for  runtime config refresh

--- hr-worker/src/main/java/com/allyson/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/allyson/hrworker/resources/WorkerResource.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,14 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
-
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-@Value("${test.config}")
-private String testeConfig;
+
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
@@ -35,8 +38,8 @@ private String testeConfig;
 
     @GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
-logger.info("CONFIG= " + testeConfig);
- return  ResponseEntity.noContent().build();
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
@@ -48,13 +51,11 @@ logger.info("CONFIG= " + testeConfig);
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
-
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
