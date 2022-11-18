1293fa5f75c0d2da1f7290e7f2c8da0e0a3886b7
Allyson Costa <costa.allyson@gmail.com>
2022-11-17 22:05:57 -0300

    Private Git repository

--- hr-config-server/src/main/java/com/allyson/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/allyson/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,21 @@
 package com.allyson.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication  {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(HrConfigServerApplication.class, args);
     }
+
 
 }
