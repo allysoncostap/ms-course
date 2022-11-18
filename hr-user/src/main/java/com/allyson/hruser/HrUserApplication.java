d7e21d44925cc22a089f00f2f02973b0dc5192ab
Allyson Costa <costa.allyson@gmail.com>
2022-11-17 22:29:51 -0300

    Configuring hr-user project

--- hr-user/src/main/java/com/allyson/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/allyson/hruser/HrUserApplication.java
@@ -2,7 +2,7 @@ package com.allyson.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
