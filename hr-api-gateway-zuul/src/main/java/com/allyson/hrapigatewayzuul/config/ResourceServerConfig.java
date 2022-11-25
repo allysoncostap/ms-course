a797fcd4fd6ec16b835e984391bf8555d1a20b16
Allyson Costa <costa.allyson@gmail.com>
2022-11-25 02:06:20 -0300

    Sercurity configs to config server

--- hr-api-gateway-zuul/src/main/java/com/allyson/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/allyson/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -8,32 +8,31 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasAnyRole;
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-@Autowired
-private JwtTokenStore tokenStore;
 
+    @Autowired
+    private JwtTokenStore tokenStore;
 
-private static final String[] PUBLIC ={"/hr-oauth/oauth/token"};
-    private static final String[] OPERATOR = {"/hr-worker/**"};
-    private static final String[] ADMIN = {"/hr-payroll/**", "/hr-user/**"};
+    private static final String[] PUBLIC = { "/hr-oauth/oauth/token" };
 
+    private static final String[] OPERATOR = { "/hr-worker/**" };
+
+    private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**", "/actuator/**", "/hr-worker/actuator/**", "/hr-oauth/actuator/**" };
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-      resources.tokenStore(tokenStore);
+        resources.tokenStore(tokenStore);
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
- http.authorizeRequests()
-         .antMatchers(PUBLIC).permitAll()
-         .antMatchers(HttpMethod.GET,OPERATOR).hasAnyRole("OPERATOR","ADMIN")
-                .antMatchers(ADMIN).hasRole("ADMIN")
-         .anyRequest().authenticated();
 
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated();
     }
 }
